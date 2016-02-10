0accfcaae77c1dd04fa8532764ba9a81369d0b60
Juuso Turunen <juusoturunen94@hotmail.com>
2016-02-10 15:46:24 +0200

    broken textures

--- TestProjects/SimpleGraphicsTest/include/BlinnPhongScene.h
+++ TestProjects/SimpleGraphicsTest/include/BlinnPhongScene.h
@@ -7,6 +7,8 @@
 #include <graphics/Mesh.h>
 #include <slmath/slmath.h>
 #include <core/Input.h>
+#include <graphics/Texture.h>
+#include <graphics/Image.h>
 
 
 class BlinnPhongScene : public Scene
@@ -27,7 +29,7 @@ public:
 
 		m_mesh = createTeapotMesh();
 		mesh = graphics::Mesh::loadFromOBJ("assets/Sphere.obj");
-
+		
 		m_sharedValues = new SharedShaderValues();
 		m_sharedValues->totalTime = 0.0f;
 		SimpleMaterialUniforms* simpleMaterialUniforms = new SimpleMaterialUniforms(shader, m_sharedValues);
@@ -35,6 +37,11 @@ public:
 		simpleMaterialUniforms->vAmbient = slmath::vec4(0.5f, 0.2f, 1.0f, 1.0f);
 		simpleMaterialUniforms->vDiffuse = slmath::vec4(0.5f, 0.2f, 1.0f, 1.0f);
 		simpleMaterialUniforms->vSpecular = slmath::vec4(1.0f, 1.0f, 1.0f, 5.0f);
+
+		graphics::Image* image = new graphics::Image(256, 256, 8);
+		image = graphics::Image::loadFromTGA("assets/CheckerBoard.tga");
+		simpleMaterialUniforms->diffuseMap->setData(image);
+
 		m_material = simpleMaterialUniforms;
 
 		checkOpenGL();
@@ -51,18 +58,53 @@ public:
 
 	virtual void update(graphics::ESContext* esContext, float deltaTime)
 	{
+		static slmath::vec3 transl = slmath::vec3(1.0f);
+		static float cameraSpeed = 3.0f;
+		static float rotation = 0.0f;
+		static slmath::vec2 lastMouseCoord = slmath::vec2(core::getMouseAxisX(), core::getMouseAxisY());
+		static float viewY = 15.0f;
+
+
 		m_totalTime += deltaTime;
 		m_sharedValues->totalTime = m_totalTime;
 
 		float fAspect = (float) esContext->width / (float) esContext->height;
 
-		m_matProjection = slmath::perspectiveFovRH(slmath::radians(45.0f), fAspect, 5.0f, 1000.0f);
+		m_matProjection = slmath::perspectiveFovRH(slmath::radians(90.0f), fAspect, 5.0f, 1000.0f);
 
-		m_matView = slmath::lookAtRH(slmath::vec3(0.0f, 70.0f, 70.0f), slmath::vec3(0.0f, 15.0f, 0.0f), slmath::vec3(0.0f, 1.0f, 0.0f));
+		m_matView = slmath::lookAtRH(slmath::vec3(0.0f, 70.0f, 70.0f), slmath::vec3(0.0f, viewY, 0.0f), slmath::vec3(0.0f, 1.0f, 0.0f));
+
+		if (core::getKeyState(core::KEY_W))
+		{
+			transl.z += cameraSpeed;
+		}
+		if (core::getKeyState(core::KEY_S))
+		{
+			transl.z -= cameraSpeed;
+		}
+		if (core::getKeyState(core::KEY_A))
+		{
+			transl.x += cameraSpeed;
+		}
+		if (core::getKeyState(core::KEY_D))
+		{
+			transl.x -= cameraSpeed;
+		}
+		if (core::getMouseButtonState(core::MOUSE_LEFT))
+		{
+			transl.x += (core::getMouseAxisX() - lastMouseCoord.x) * 0.3f;
+			transl.y += (core::getMouseAxisY() - lastMouseCoord.y) * 0.3f;
+		}
+		if (core::getMouseButtonState(core::MOUSE_RIGHT))
+		{
+			rotation += (core::getMouseAxisX() - lastMouseCoord.x) * 0.003f;
+			transl.x -= (core::getMouseAxisX() - lastMouseCoord.x) * 0.3f;
+			viewY -= (core::getMouseAxisY() - lastMouseCoord.y) * 0.3f;
+		}
 
 		m_matModel = slmath::rotationX(-PI*0.5f);
-		m_matModel = slmath::rotationY(m_totalTime/3.0f) * m_matModel;
-		m_matModel = slmath::translation(slmath::vec3(cos(m_totalTime), sin(m_totalTime), sin(m_totalTime))) * m_matModel;
+		m_matModel = slmath::rotationY(rotation) * m_matModel;
+		m_matModel = slmath::translation(transl) * m_matModel;
 
 		m_sharedValues->matModel = m_matModel;
 		m_sharedValues->matView = m_matView;
@@ -76,8 +118,11 @@ public:
 		m_sharedValues->matNormal = matNormal;
 		m_sharedValues->matModelViewProj = matModelViewProj;
 
-		m_sharedValues->lightPos = slmath::vec3((float) core::getMouseAxisX() - esContext->width/2.0f, (float) core::getMouseAxisY() + esContext->height/2.0f, -20.0f);
-		m_sharedValues->camPos = slmath::vec3(0.0f, 70.0f, 70.0f);
+		m_sharedValues->lightPos = slmath::vec3((float) core::getMouseAxisX() - esContext->width / 2.0f, (float) core::getMouseAxisY() + esContext->height / 2.0f, -20.0f);
+		m_sharedValues->camPos = slmath::vec3(0.0f, (float) core::getMouseAxisY(), (float) core::getMouseAxisX());
+		m_sharedValues->camPos = transl;
+
+		lastMouseCoord = slmath::vec2(core::getMouseAxisX(), core::getMouseAxisY());
 	}
 
 
@@ -85,7 +130,7 @@ public:
 	{
 		glViewport(0, 0, esContext->width, esContext->height);
 
-		glClearColor(0.0f, 0.8f, 0.8f, 1.0f);
+		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		checkOpenGL();
 
--- TestProjects/SimpleGraphicsTest/include/SimpleMaterialUniforms.h
+++ TestProjects/SimpleGraphicsTest/include/SimpleMaterialUniforms.h
@@ -2,6 +2,7 @@
 
 #include "MyMaterials.h"
 #include <graphics\Shader.h>
+#include <graphics\Texture.h>
 
 class SimpleMaterialUniforms : public graphics::ShaderUniforms
 {
@@ -9,6 +10,7 @@ public:
 	slmath::vec4 vAmbient;
 	slmath::vec4 vDiffuse;
 	slmath::vec4 vSpecular;
+	graphics::Texture2D* diffuseMap;
 
 public:
 	SimpleMaterialUniforms(graphics::Shader* shader, SharedShaderValues* sharedValues = 0) : ShaderUniforms(shader), 
@@ -23,15 +25,22 @@ public:
 		m_materialAmbientLoc = glGetUniformLocation(shader->getProgram(), "g_Material.vAmbient");
 		m_materialDiffuseLoc = glGetUniformLocation(shader->getProgram(), "g_Material.vDiffuse");
 		m_materialSpecularLoc = glGetUniformLocation(shader->getProgram(), "g_Material.vSpecular");
+		m_diffuseMapLoc = glGetUniformLocation(shader->getProgram(), "s_diffuseMap");
 	}
 
 	virtual void bind(graphics::Shader* shader)
 	{
 		shader->bind();
 		m_globalShaderUniforms->bind(shader);
+
+		glActiveTexture(GL_TEXTURE0 + 0);
+		glBindTexture(GL_TEXTURE_2D, diffuseMap->getTextureId());
+
 		glUniform4fv(m_materialAmbientLoc, 1, &vAmbient[0]);
 		glUniform4fv(m_materialDiffuseLoc, 1, &vDiffuse[0]);
 		glUniform4fv(m_materialSpecularLoc, 1, &vSpecular[0]);
+
+		glUniform1i(m_diffuseMapLoc, 0);
 	}
 
 private:
@@ -39,4 +48,5 @@ private:
 	GLint m_materialAmbientLoc;
 	GLint m_materialDiffuseLoc;
 	GLint m_materialSpecularLoc;
+	GLint m_diffuseMapLoc;
 };
